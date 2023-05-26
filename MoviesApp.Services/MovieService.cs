3f57f3f038176302b2d51fcdd79440115ee30ae5
Agostina <75433684+Agostina-mr@users.noreply.github.com>
2023-05-26 10:21:59 +0200

    styling

--- MoviesApp.Services/MovieService.cs
+++ MoviesApp.Services/MovieService.cs
@@ -15,7 +15,7 @@ public class MovieService : IMovieService
 
     public async Task<MovieResponseDto> GetMovieAsync(string userId, int movie_id)
     {
-        return await _moviesClient.GetMovieAsync(movie_id);
+        return await _moviesClient.GetMovieAsync(movie_id, userId);
     }
 
     public Task AddFavoriteMovieAsync(FavoritesDto favoriteMovie)

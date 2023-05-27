400dcd3a144ef4e0f6356e8099ac007ca36c3a28
Agostina <75433684+Agostina-mr@users.noreply.github.com>
2023-05-27 15:40:32 +0200

    fix?

--- MoviesApp.Services/MovieService.cs
+++ MoviesApp.Services/MovieService.cs
@@ -45,7 +45,7 @@ public class MovieService : IMovieService
 
     public Task<MovieCreditsResponseDto> GetMovieCreditsAsync(int movieId)
     {
-        return _moviesClient.GetMovieCreditsAsync(null, movieId);
+        return _moviesClient.GetMovieCreditsAsync(string.Empty, movieId);
     }
 
     public async Task<RatingDto> GetMovieRatingAsync(int movieId)
